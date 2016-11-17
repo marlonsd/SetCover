95e39c4f16069eca5701c53d8585ff33575f8e8c
marlonsd <marlonsdias@gmail.com>
2016-11-17 14:47:07 -0200

    greedy and first fit heuristics added

---
+++ constructive_heuristic.py
@@ -0,0 +1,56 @@
+import math, sys, glob
+
+from time import time
+
+import numpy as np
+
+def greedy_selection(cols, lines):
+	in_set = set()
+
+	n_sets_chosen = 0
+
+	while(not (in_set == cols) and len(lines) > 0):
+		uniques = []
+
+		for line in lines:
+			unique = len(set(line) - in_set)
+			uniques.append(unique)
+
+		uniques = np.argsort(uniques)
+
+		chosen = uniques[-1]
+
+		in_set.update(lines[chosen])
+
+		lines.pop(chosen)
+
+		n_sets_chosen+=1
+
+	return (in_set == cols), n_sets_chosen
+
+def first_fit_selection(cols, lines):
+	in_set = set()
+
+	n_sets_chosen = 0
+
+	while(not (in_set == cols) and len(lines) > 0):
+
+		i = 0
+		condition = not (len(lines[i] - in_set))
+		while (condition):
+			i+=1
+
+			if (i >= len(lines)):
+				condition = False
+				i = len(lines) - 1
+			else:
+				condition = not (len(lines[i] - in_set))
+
+		# print lines[i] - in_set
+		in_set.update(lines[i])
+
+		lines.pop(i)
+
+		n_sets_chosen+=1
+
+	return (in_set == cols), n_sets_chosen
