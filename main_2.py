6d6df9e9a7dce849abb6b36880f2792439b4a471
Marlon Dias <marlonsdias@gmail.com>
2016-12-04 14:58:50 -0200

    finishing grasp

---
+++ main_2.py
@@ -0,0 +1,87 @@
+import argparse, math, sys, glob
+
+import numpy as np
+
+from copy import copy
+from time import time
+
+from metaheuristics import grasp
+
+if (__name__ == '__main__'):
+
+	### Arguments Treatment ###
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-fp","--folder_path", type=str, default='instances/clean',
+						help="Folder where SCP instances are located (Default: instances).")
+
+	parser.add_argument("-f","--file_format", type=str, default='txt',
+						help="Format of file where SCP instances are stored (Default: txt).")
+
+	# n_iterations=500, alpha=0.9, p=0.75, max_flips=None, seed=None)
+
+	parser.add_argument("-n","--n_iterations", type=int, default=500,
+						help="Maximum number of iteration executed by the metaheuristics (Default: 500).")
+
+	parser.add_argument("-a","--alpha", type=float, default=0.9,
+						help="Alpha parameter for the randomized greedy algorithm (Default: 0.9).")
+
+	parser.add_argument("-p","--prob", type=float, default=0.75,
+						help="Chance to choose best fit instead of random fit for the GRASP local improviment (Default: 0.75).")
+
+	parser.add_argument("-m","--max_flips", type=int, default=0,
+						help="Maximum number of flips in GRASP's local improviment; 0 means it is 10 times the size of sets set (Default: 0).")
+
+	parser.add_argument("-s","--seed", type=int, default=0,
+						help="Seed for the random algorithms; 0 means no seed being used (Default: 0).")
+
+	# Parsing args
+	args = parser.parse_args()
+
+	path = args.folder_path
+	file_format = args.file_format
+
+	n_iterations = args.n_iterations
+	alpha = args.alpha
+	p = args.prob
+	max_flips = args.max_flips
+	seed = args.seed
+
+	if max_flips <= 0:
+		max_flips = None
+
+
+	if seed <= 0:
+		seed = None
+
+	files = sorted(glob.glob(path+"/*."+file_format))
+
+	print "filename", "," , "grasp", "," , "time (seg)"#, "," , "first_fit", "," , "time (seg)", "," , "error"
+
+	for file in files:
+		f = open(file, 'r')
+
+		data = f.read().splitlines()
+
+		problem_name = file.split("/")[-1].split(".")[0]
+		number_instances, max_value = map(int,data[0].split(" "))
+
+		print problem_name, "," ,
+
+		cols = set(range(1,max_value+1))
+
+		lines = []
+
+		error = 0
+
+		for line in data[1:]:
+			lines.append(set(map(int,line.split(" "))))
+
+		t0 = time()
+
+		condition, n = grasp(copy(cols),copy(lines), n_iterations=n_iterations, alpha=alpha, p=p, max_flips=max_flips, seed=seed)
+		print n, "," ,(time() - t0), "," ,
+
+		# t0 = time()
+		# condition, n = first_fit_selection(copy(cols),copy(lines))
+		# print n, "," , (time() - t0), "," ,
--- metaheuristics.py
+++ metaheuristics.py
@@ -12,23 +12,93 @@ def is_feasible(cols, lines, solution):
 	return set(cols) == sol_set
 
 def update_solution(solution, new_solution):
-	if solution[0] == None:
-		return new_solution
+	if solution == None:
+		return new_solution, True
 
-	if new_solution[0] < solution[0]:
-		return new_solution
+	if len(new_solution) < len(solution):
+		return new_solution, True
 
-	return solution
+	return solution, False
 
+# Randomized greed algorithm
 def constructive_phase(cols, lines, alpha):
-	pass
+	in_set = set()
+
+	cover_set = []
+
+	candidates = copy(lines)
+
+	while(not (in_set == cols) and len(candidates) > 0):
+		uniques = []
+
+		for line in candidates:
+			unique = len(set(line) - in_set)
+			uniques.append(unique)
+
+		uniques = np.argsort(uniques)
+
+		# Select alpha best
+		possible_selection = uniques[len(uniques) - int(len(uniques)*alpha):]
+
+		# Picks one randomly
+		chosen = np.random.choice(possible_selection)
+
+		cover_set.append(lines.index(candidates[chosen]))
+
+		in_set.update(candidates[chosen])
+
+		candidates.pop(chosen)
+
+	return sorted(cover_set)
 
 def local_improvement(cols, lines, solution, p, max_flips):
-	def best_flip(s):
-		pass
 
-	def random_flip(s):
-		pass
+	# Given a solution, finds the best solution (smallest)
+	# reachable from the given solution
+	def best_flip(cols, lines, s):
+		best_solutions = [copy(s)]
+		best_solution = copy(s)
+
+		while (len(best_solutions) > 0):
+			local_solution = best_solutions.pop(0)
+
+			for i in range(len(local_solution)):
+				short_solution = copy(local_solution)
+				short_solution.pop(i)
+
+				if (is_feasible(cols, lines, short_solution)):
+					if (len(short_solution) < len(best_solution)):
+						# print "\t\tBest Solution size", len(short_solution)
+						# best_solutions.insert(0, copy(short_solution))
+						best_solutions.append(copy(short_solution))
+						best_solution = copy(short_solution)
+
+		return sorted(best_solution)
+
+
+	# Randomly selects a set and either add or remove it from solution
+	# Until new_solution is feasible
+	def random_flip(cols, lines, s):
+		new_solution = copy(s)
+		feasible = False
+
+		possibilites = range(len(lines))
+
+		saved_solution = copy(s)
+
+		while (not feasible):
+			chosen = np.random.choice(possibilites)
+
+			if (chosen in new_solution):
+				new_solution.pop(new_solution.index(chosen))
+			else:
+				new_solution.append(chosen)
+
+			feasible = is_feasible(cols, lines, new_solution)
+			saved_solution = copy(new_solution)
+			new_solution = copy(s)
+
+		return saved_solution
 
 	if max_flips == None:
 		max_flips = 10*len(lines)
@@ -36,26 +106,34 @@ def local_improvement(cols, lines, solution, p, max_flips):
 	best_solution = None
 
 	for i in range(max_flips):
+		# print "\tImproviment Iteration", i,
 		if np.random.random() < p:
-			solution = best_solution(solution)
+			# print "Best Flip"
+			solution = best_flip(cols, lines, solution)
 		else:
-			solution = random_solution(solution)
+			# print "Random Flip"
+			solution = random_flip(cols, lines, solution)
 
-		best_solution = update_solution(best_solution, solution)
+		best_solution, cond = update_solution(best_solution, solution)
+		if not cond:
+			return best_solution
 
 	return best_solution
 
 def grasp(cols, lines, n_iterations=500, alpha=0.9, p=0.75, max_flips=None, seed=None):
 
-	best_solution = (None, None)
+	best_solution = None
 
 	if not seed == None:
 		np.random.seed(seed)
 
+	print 
+
 	for k in range(n_iterations):
+		print "Grasp Iteration", k
 		solution = constructive_phase(cols, lines, alpha)
 		solution = local_improvement(cols, lines, solution, p, max_flips)
 
-		best_solution = update_solution(best_solution, solution)
+		best_solution, _ = update_solution(best_solution, solution)
 
 	return best_solution
